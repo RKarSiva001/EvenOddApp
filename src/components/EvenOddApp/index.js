fd9dfc23119e6949ad1af0f6a7513a5b51e87d81
RKarSiva001 <rkarsivasundaram2001@gmail.com>
2024-02-05 08:06:22 +0000

    Even Odd App

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -1,29 +1,36 @@
-// Write your code here
 import {Component} from 'react'
+
 import './index.css'
 
 class EvenOddApp extends Component {
-  state = {
-    count: 0,
-  }
+  state = {count: 0}
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
 
   onIncrement = () => {
-    this.setState({
-      count: Math.floor(Math.random() * 100),
-    })
+    const randomNumber = this.getRandomNumber()
+
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
   }
 
   render() {
-    const {count, isEven} = this.state
+    const {count} = this.state
+    const displayText = count % 2 === 0 ? 'Even' : 'Odd'
 
     return (
-      <div className="bg-container">
-        <h1>Count {count}</h1>
-        <p>Count is {count % 2 === 0 ? 'Count is Even' : 'Count is Odd'}</p>
-        <button type="button" onClick={this.onIncrement}>
-          Increment
-        </button>
-        <p>*Increase By Random Number Between 0 to 100</p>
+      <div className="app-container">
+        <div className="count-container">
+          <h1 className="count">Count {count}</h1>
+          <p className="number-category">Count is {displayText}</p>
+          <button
+            type="button"
+            className="increment-button"
+            onClick={this.onIncrement}
+          >
+            Increment
+          </button>
+          <p className="note">*Increase By Random Number Between 0 to 100</p>
+        </div>
       </div>
     )
   }
